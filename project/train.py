a365a5537aee2e0e65ac10129a4caaef85d0f325
Adam Bujak <adamjbujak@gmail.com>
2022-04-07 16:59:49 -0400

    add shuffle and reduce to train

--- project/train.py
+++ project/train.py
@@ -1,10 +1,24 @@
 import numpy as np
 import pandas as pd
 
+from sklearn.utils import shuffle
+def shuffle_data(X, y):
+    X, y = shuffle(X, y, random_state=0)
+    return X, y
+
 #2 Importing the dataset
 dataset = pd.read_csv('data.csv')
 X = dataset.iloc[:,0:18].values.astype(float)
 y = dataset.iloc[:,18].values.astype(float)
+
+print(X)
+print(y)
+X, y = shuffle_data(X,y)
+
+X = X[0:10000]
+y = y[0:10000]
+print(X)
+print(y)
 
 #3 Feature Scaling
 #from sklearn.preprocessing import StandardScaler
