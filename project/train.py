b9c776936e99ebdf0cdc2f111f4a1da2ad1bb48f
Adam Bujak <adamjbujak@gmail.com>
2022-04-07 16:51:16 -0400

    fix train.py

--- project/train.py
+++ project/train.py
@@ -1,11 +1,10 @@
 import numpy as np
-import matplotlib.pyplot as plt
 import pandas as pd
 
 #2 Importing the dataset
 dataset = pd.read_csv('data.csv')
 X = dataset.iloc[:,0:18].values.astype(float)
-y = dataset.iloc[:,18:19].values.astype(float)
+y = dataset.iloc[:,18].values.astype(float)
 
 #3 Feature Scaling
 #from sklearn.preprocessing import StandardScaler
@@ -30,5 +29,5 @@ from joblib import dump
 #dump(sc_y, 'std_scaler_y.bin', compress=True)
 dump(regressor, 'regressor.joblib')
 
-print("model score: ", regressor.score(x_train, y_train))
+print("model score: ", regressor.score(x_test, y_test))
 
